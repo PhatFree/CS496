729108ae8cd8ab7c7c4dde506a5242a8d385c3a8
root <jeffbk95@gmail.com>
2018-04-22 22:04:03 +0000

    Proper chunk sizes from 1-128 now

--- OMP/perf.py
+++ OMP/perf.py
@@ -16,13 +16,13 @@ def main():
             rand = "results/ranD/" + str(i) +"_thread-"+ str(j)  + "ranD.txt"
 
                         #call perf stat
-#			subprocess.call(["perf", "stat", "--per-core", "-a", "-o", stxt, "./static", str(j)])
+	    subprocess.call(["perf", "stat", "--per-core", "-a", "-o", stxt, "./static", str(j)])
             parsefile(stxt)
-#			subprocess.call(["perf", "stat", "--per-core", "-a", "-o", dtxt, "./dynamic", str(j)])
+	    subprocess.call(["perf", "stat", "--per-core", "-a", "-o", dtxt, "./dynamic", str(j)])
             parsefile(dtxt)
-#			subprocess.call(["perf", "stat", "--per-core", "-a", "-o", gtxt, "./guided", str(j)])
+	    subprocess.call(["perf", "stat", "--per-core", "-a", "-o", gtxt, "./guided", str(j)])
             parsefile(gtxt)
-#			subprocess.call(["perf", "stat", "--per-core", "-a", "-o", rand, "./ran_dynamic", str(j)])
+	    subprocess.call(["perf", "stat", "--per-core", "-a", "-o", rand, "./ran_dynamic", str(j)])
             parsefile(rand)
 
             #chunksize
@@ -32,9 +32,9 @@ def main():
                         chunk = "results/chunk/" + str(i) +"_thread-"+ str(j)+ "_cunksize-"+ str(c)  + "C_D.txt"
                         #random
                         chunkrand = "results/chunk/" + str(i) +"_thread-"+ str(j)+"_chunkszie-"+ str(c)+ "ranC_D.txt"
-#			            subprocess.call(["perf", "stat", "--per-core", "-a", "-o", chunk, "./chunk_dynamic", str(j),str(2**c)])
+	                subprocess.call(["perf", "stat", "--per-core", "-a", "-o", chunk, "./chunk_dynamic", str(j),str(2**c)])
                         parsefile(chunk)
-#			            subprocess.call(["perf", "stat", "--per-core", "-a", "-o", chunkrand, "./chunk_ran_dynamic", str(j),str(2**c)])
+   	                subprocess.call(["perf", "stat", "--per-core", "-a", "-o", chunkrand, "./chunk_ran_dynamic", str(j),str(2**c)])
                         parsefile(chunkrand)
 
 def parsefile(filename):
